3ff193fdfa3af898559150ee743ab0dbb861319f
github Tanio1123 <yuyajapan1123@gmail.com>
2019-11-25 17:34:53 +0800

    初期

--- js/app.js
+++ js/app.js
@@ -1,14 +1,21 @@
-$(function () {
-
+$(function() {
   // フロートヘッダーメニュー
-  var targetHeight = $('.js-float-menu-target').height();
-  $(window).on('scroll', function() {
-    $('.js-float-menu').toggleClass('float-active', $(this).scrollTop() > targetHeight);
+  var targetHeight = $(".js-float-menu-target").height();
+  $(window).on("scroll", function() {
+    $(".js-float-menu").toggleClass(
+      "float-active",
+      $(this).scrollTop() > targetHeight
+    );
   });
 
   // SPメニュー
-  $('.js-toggle-sp-menu').on('click', function () {
-    $(this).toggleClass('active');
-    $('.js-toggle-sp-menu-target').toggleClass('active');
+  $(".js-toggle-sp-menu").on("click", function() {
+    $(this).toggleClass("active");
+    $(".js-toggle-sp-menu-target").toggleClass("active");
+  });
+
+  $(".js-menu-link-remove").on("click", function() {
+    $(this).toggleClass("active");
+    $(".js-toggle-sp-menu-target").toggleClass("active");
   });
 });
